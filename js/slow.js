9a9a4bbc8d9e9f140ac4840929914a8f9bcec3a7
sam ear <sam.ear@gmail.com>
2023-08-10 19:41:54 -0700

    update

---
+++ js/slow.js
@@ -0,0 +1,21 @@
+var t = 10000;
+h1Str = "Good Bye";
+console.log("waiting... for " + t +"ms. before changing header.")
+//document.querySelector('p').innerHTML="running index.js";
+
+//setTimeout(myGreeting, t);
+
+/* function myGreeting() {
+    document.querySelector("h1").innerHTML = h1Str;
+    document.querySelector('h1').style.color='red';
+    console.log("done.")
+} */
+
+// same as above, this is called anonymous function
+setTimeout(function() {
+    //document.querySelector("h1").innerHTML=h1Str;
+    document.querySelector('a').style.color='red';
+    console.log("done.")}, t)
+
+//document.querySelector("a").style.color='red';
+//document.querySelectorAll("a")[1].style.color='green';
