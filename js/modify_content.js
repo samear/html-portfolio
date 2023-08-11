509724c9c194b58e5cf0b0d61eb960579b062cce
sam ear <sam.ear@gmail.com>
2023-08-11 14:33:30 -0700

    update

--- js/modify_content.js
+++ js/modify_content.js
@@ -1,2 +1,3 @@
 document.querySelector('a').innerHTML="x=" + timeWaster();
+document.querySelector('div[id="templatemo_logo"]').style.background='yellow';
 document.querySelector('a').style.color='#800080';
