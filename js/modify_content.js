9e47d00b632ff6bb7a69ce396e530c6051331f49
sam ear <sam.ear@gmail.com>
2023-08-11 14:47:40 -0700

    remove timeWast call from modify_content.js

--- js/modify_content.js
+++ js/modify_content.js
@@ -1,3 +1,4 @@
-document.querySelector('a').innerHTML="x=" + timeWaster();
+//document.querySelector('a').innerHTML="x=" + timeWaster();
+document.querySelector('a').innerHTML="x=";
 document.querySelector('div[id="templatemo_logo"]').style.background='yellow';
 document.querySelector('a').style.color='#800080';
